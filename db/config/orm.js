5bd6cb34fbac600cd92f0d35292a7329a5427a9d
Aaron Sims <aaron@adsimsllc.com>
2020-08-14 18:32:55 -0400

    loading ORM methods

---
+++ db/config/orm.js
@@ -0,0 +1,33 @@
+const connection = require("../config/connection.js");
+const orm = {
+  selectAll: function (tableInput, cb) {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  insertOne: function (tableInput, data, cb) {
+    const queryString = "INSERT INTO " + tableInput + " SET ?;";
+    connection.query(queryString, data, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  updateOne: function (tableInput, data, update, cb) {
+    const queryString = "UPDATE " + tableInput + " SET ? WHERE ?;";
+    connection.query(queryString, [data, update], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+module.exports = orm;
